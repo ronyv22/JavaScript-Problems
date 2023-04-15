df34171134921cd1837b996d2685d34e1ee81de6
ronyv22 <ronyvic84@gmail.com>
2023-04-15 17:37:30 -0400

    updated solution with notes

--- functions-problems/problem-1/script.js
+++ functions-problems/problem-1/script.js
@@ -1,36 +1,49 @@
-//Write a Javascript function that accepts a string as a 
-//parameter and returns the number of vowels in that string
-
+//Write a JavaScript function that accepts a string as a parameter 
+//and find the longest word within the string.
 
 
 /*
-step 1: define a JavaScript function that accept a string as a parameter
-step 2: split the string passed in, turn it into an array of each individual letter,
-store it in a variable called arrayOfLetters
-step 3: define a variable called numberOfVowels and set the initial value to be equal to 0;
-step 4: define a variable that defines what a vowel is, an array of vowels
-step 5: start looping through our array of letters
-step 6: chack if the current letter in our loop is a vowel
-step 7: increase the count variable by one for every letter that is a vowel
-step 8: return the count of vowels
+step 1: define a JavaScript function that accepts a string as a parameter
+step 2: split the string passed in, turn it into an array of words,
+store it in a variable called arrayOfWords
+step 3: define a variable called longestWord and set the initial value as an empty string;
+step 4: start looping through our array of words
+step 5: check if the current word in our array is longer than current value of longestWord variable
+step 6: if it is, update the value of current word 
+step 7: return the longest word in the string
 */
 
 
-function countVowels(str) {
-	let arrayOfLetters = str.split("");
-	let numberOfVowels = 0;
-	const arrayOfVowels = ["a", "e", "i", "o", "u"];
+// Initialized a function called findLongestWord to pass a string as a parameter
 
+function findLongestWord(str) {
 
-	for (let = 0; i < arrayOfLetters.length; i++) {
-		if (vowels.indexOf(arrayOfLetters[i].toLowerCase()) !== -1) {
-			numberOfVowels++;
+// Initialized a variable called arrayOfWords to split the string into an array of words by the space
+//	between them
+
+	const arrayOfWords = str.split(" ");
+
+// Initialized a variable called longestWord and set the value as an empty string as placeholder that 
+// equates to the value of 0. It will be used as a starting value to compare within the loop
+
+	let longestWord = "";
+
+// Initialized a for loop with an if statement to check whether the current word in arrayOfWords is
+// longer than the longestWord variable. If so, it will update the value of the longestWord to the
+// current word	
+
+	for(let i = 0; i < arrayOfWords.length; i++) {
+		if(arrayOfWords[i].length > longestWord.length) {
+			longestWord = arrayOfWords[i];
 		}
 	}
-	return numberOfVowels;
-}
 
-console.log(countVowels("Customers who bought this item also bought these drawings"));
+// After the loop, the longestWord variable will be returned, containing the longest
+// word found in the string
 
+	return longestWord;
+}							
+
+console.log(findLongestWord("Why is there a rabbit in that hat?")); // "rabbit" 
 
 
