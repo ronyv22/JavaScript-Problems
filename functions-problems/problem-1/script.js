eacc6384e311b570b537c542c7ab429a8e2aeb65
ronyv22 <ronyvic84@gmail.com>
2023-07-04 10:41:19 -0400

    Updated solution

--- functions-problems/problem-1/script.js
+++ functions-problems/problem-1/script.js
@@ -4,31 +4,30 @@
 
 /*
 step 1: define a JavaScript function that accepts a string as a parameter
-step 2: split the string passed in, turn it into an array of words,
+step 2: split the string, turn it into an array of individual words,
 store it in a variable called arrayOfWords
 step 3: define a variable called longestWord and set the initial value as an empty string;
 step 4: start looping through our array of words
-step 5: check if the current word in our array is longer than current value of longestWord variable
+step 5: check if the current word in our array is longer than current value in the longestWord variable
 step 6: if it is, update the value of current word 
 step 7: return the longest word in the string
 */
 
 
-// Initialized a function called findLongestWord to pass a string as a parameter
-// Initialized a variable called arrayOfWords to split the string into an array of words by the space
-//	between them
-// Initialized a variable called longestWord and set the value as an empty string as placeholder that 
-// equates to the value of 0. It will be used as a starting value to compare within the loop
-// Initialized a for loop with an if statement to check whether the current word in arrayOfWords is
+// Intialized a function called findLongestWord for the string value that is passed when function is called
+// Initialized a variable called arrayOfWords to split up the string into an array by the space
+// between them
+// Initialized a variable called longestWord and set the value as an empty string as placeholder with
+// no value
+// Initialized a for loop to check if whether the current word in arrayOfWords array is
 // longer than the longestWord variable. If so, it will update the value of the longestWord to the
 // current word	
-// After the loop, the longestWord variable will be returned, containing the longest
+// After the loop, the longestWord variable will be returned, printing the longest
 // word found in the string
+
 
 function findLongestWord(str) {
 	const arrayOfWords = str.split(" ");
-
-
 	let longestWord = "";
 
 
